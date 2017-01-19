3278988134b456ea4556c162dee4068a91398214
jackiemauro <jacqueline.amauro@gmail.com>
2017-01-19 13:11:58 -0500

    added reconstitution fn
    Also checking the function which reconstitutes the covariance matrix within loglik function

--- covTrans_checks.R
+++ covTrans_checks.R
@@ -29,3 +29,52 @@ make.covTrans(list(rho = mat[1,2], y_sd = mat[2,2],
                    tau0 = c(mat[1,3:4]),tau1 = c(mat[2,3:4]))
               , num_endog = 2, gamma = c(0,0), beta = c(0,0),
               option = "parameters")
+
+#comparison
+t1=make.covTrans(list(rho = mat[1,2], y_sd = mat[2,2],
+                   endog_sd = c(mat[3,3],mat[4,4]),
+                   tau0 = c(mat[1,3:4]),tau1 = c(mat[2,3:4]))
+              , num_endog = 2, gamma = c(1,2), beta = c(3,3),
+              option = "parameters")
+
+t2=make.covTrans1(rho = mat[1,2], y_sd = mat[2,2],
+               endog_sd = c(mat[3,3],mat[4,4]),
+               tau0 = c(mat[1,3:4]),tau1 = c(mat[2,3:4]),
+               gamma = c(1,2), beta = c(3,3))
+
+t1==t2
+
+t1=make.covTrans(list(rho = 3, y_sd = 2,
+                      endog_sd = c(2,3),
+                      tau0 = c(3,4),tau1 = c(5,6))
+                 , num_endog = 2, gamma = c(1,2), beta = c(3,3),
+                 option = "parameters")
+
+t2=make.covTrans1(rho = 3, y_sd = 2,
+                  endog_sd = c(2,3),
+                  tau0 = c(3,4),tau1 = c(5,6),
+                  gamma = c(1,2), beta = c(3,3))
+
+t1==t2v
+
+########## checking reconstituting fn  ##############
+myChol = T
+mat = Posdef(4)
+st = mat/mat[1,1]
+c = chol(st)
+t = c[upper.tri(c,diag = T)][-1]
+
+st2 = reconstitute.cov(vals =t, num = 1)
+st2 = reconstitute.cov(vals =t, num = 2)
+st2 == st
+
+#no cholesky
+myChol = F
+mat = matrix(c(1,2,3,
+               2,4,5,
+               3,5,9),
+             ncol = 3, byrow = T)
+t = mat[upper.tri(mat,diag = T)][-1]
+
+st2 = reconstitute.cov(vals =t, num = 1)
+st2 == mat
